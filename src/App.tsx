37483d7be838c9e010ea86a84f67863739445677
giorgiBerika <giorgi.berikashvili.3@iliauni.edu.ge>
2023-09-20 15:54:34 +0400

    Adding background urls to the tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
 import { useState } from 'react'
 import './App.css'
 
+import desktopBgDay from './assets/desktop/bg-image-daytime.jpg';
+
 function App() {
 
+ const onClickHandler = () => {
+    const divOne = document.querySelector('.div-1') as HTMLElement;
+
+    if (divOne) {
+
+      divOne.style.height = (divOne.style.height === '100%') ? '50%' : '100%';
+    }
+  };
   return (
     <>
-        Main App
+      <div className={`
+        w-screen
+        min-h-screen
+        h-screen
+        bg-desktopDay
+        bg-center
+        bg-no-repeat
+        bg-cover
+        h-full
+        `}>
+        {/* border
+        border-rose-500 */}
+        <div className='div-1 div'>
+          div 1
+        </div>
+        <div className='div-2 div'>
+          div 2
+        </div>
+
+        <button type='button' onClick={onClickHandler}>click it</button>
+        
+      </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ export default {
   ],
   theme: {
     extend: {
-
+      backgroundImage: {
+        desktopDay: "url('./src/assets/desktop/bg-image-daytime.jpg')",
+        desktopNight: "url('./src/assets/desktop/bg-image-nighttime.jpg')",
+        tabletDay: "url('./src/assets/tablet/bg-image-daytime.jpg')",
+        tabletNight: "url('./src/assets/tablet/bg-image-nighttime.jpg')",
+        mobileDay: "url('./src/assets/mobile/bg-image-daytime.jpg')",
+        mobileNight: "url('./src/assets/mobile/bg-image-nighttime.jpg')",
+      },
       colors: {
         colorBlackFirst: '#000000',
         colorBlackSecond: '#303030',
